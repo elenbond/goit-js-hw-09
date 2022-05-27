4024fbdbca1526a6748a4a37cf3066847223a7cb
Olena Bondiuchna <93439766+elenbond@users.noreply.github.com>
2022-05-27 21:23:39 +0300

    added task2&task3

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,77 @@
-// Описаний в документації
 import flatpickr from "flatpickr";
-// Додатковий імпорт стилів
-// import "flatpickr/dist/flatpickr.min.css";
+import "flatpickr/dist/flatpickr.min.css";
+import Notiflix from "notiflix";
 
 const inputDate = document.querySelector("#datetime-picker");
-console.log(inputDate);
+const buttonStart = document.querySelector('button[data-start]');
+const outputDays = document.querySelector("[data-days]");
+const outputHours = document.querySelector("[data-hours]");
+const outputMinutes = document.querySelector("[data-minutes]");
+const outputSeconds = document.querySelector("[data-seconds]");
+
+buttonStart.disabled = true;
+const DELAY = 1000;
+let timerId = null;
+
+function convertMs(ms) {
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  const days = Math.floor(ms / day);
+  const hours = Math.floor((ms % day) / hour);
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+    onClose(selectedDates) {
+        const currentDate = options.defaultDate.getTime();
+        const selectedDate = selectedDates[0].getTime();
+        if (selectedDate < currentDate) {
+            buttonStart.disabled = true;
+            // window.alert("Please choose a date in the future");
+            Notiflix.Notify.failure("Please choose a date in the future");
+        } else {
+            buttonStart.disabled = false;
+        }
+  },
+};
+
+function addLeadingZero(value) {
+    return String(value).padStart(2, '0');
+}
+
+const addingZeroToString = ({ days, hours, minutes, seconds }) => {
+    outputDays.textContent = addLeadingZero(days);
+    outputHours.textContent = addLeadingZero(hours);
+    outputMinutes.textContent = addLeadingZero(minutes);
+    outputSeconds.textContent = addLeadingZero(seconds);
+    
+    buttonStart.disabled = true;
+}
+
+const flatp = flatpickr(inputDate, options);
+
+const stopTimer = ({ days, hours, minutes, seconds }) => {
+    if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0) {
+        return addingZeroToString({days: 0, hours: 0, minutes: 0, seconds: 0})
+    }
+}
+
+const onButtonStartClick = () => {
+    const date = new Date();
+    timerId = setInterval(() => {
+        const currentTime = convertMs(flatp.selectedDates[0] - Date.now());
+        addingZeroToString(currentTime);
+        stopTimer(currentTime);
+    }, DELAY);
+}
+
+buttonStart.addEventListener("click", onButtonStartClick);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,47 @@
+import Notiflix from "notiflix";
+
+const form = document.querySelector(".form");
+const elements = {};
+const inputedValues = (form) => {
+  [...form.elements].forEach(({name, value}) => (elements[name] = Number(value))); 
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        // Fulfill
+        resolve({ position: position, delay: delay });
+      } else {
+        // Reject
+        reject({ position: position, delay: delay });
+      }
+    }, delay);
+  });
+  return promise;
+}
+
+const generatePromise = () => {
+  const { delay, step, amount } = elements;
+  let totalDelay = delay;
+  for (let i = 1; i <= amount; i++) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    totalDelay += step;
   }
 }
+
+const onFormSubmit = (event) => {
+  event.preventDefault();
+  inputedValues(event.currentTarget);
+  generatePromise();
+  event.currentTarget.reset();
+}
+
+form.addEventListener('submit', onFormSubmit);
